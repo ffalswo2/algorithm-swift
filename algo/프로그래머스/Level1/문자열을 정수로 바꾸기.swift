7986b65c12503f66ab427fc3e2e3bb36b2f6922f
Kim Minjae <ffalswo2@naver.com>
2022-08-14 17:41:14 +0900

    프로그래머스 Level1 문자열을 정수로 바꾸기

---
+++ algo/프로그래머스/Level1/문자열을 정수로 바꾸기.swift
@@ -0,0 +1,14 @@
+//
+//  문자열을 정수로 바꾸기.swift
+//  algo
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12925
+//  Created by 김민재 on 2022/08/14.
+//
+
+import Foundation
+
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
+
+solution("-1234")
