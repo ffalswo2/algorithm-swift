f19775e4c5a3753d947ba7b8cf4f0fdec9e428a1
Kim Minjae <ffalswo2@naver.com>
2022-08-17 21:25:23 +0900

    프로그래머스 Level1 짝수와 홀수

---
+++ algo/프로그래머스/Level1/짝수와 홀수.swift
@@ -0,0 +1,13 @@
+//
+//  짝수와 홀수.swift
+//  algo
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12937
+//  Created by 김민재 on 2022/08/17.
+//
+
+import Foundation
+
+
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
