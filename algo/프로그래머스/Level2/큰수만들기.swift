90db8b1248e42ccaaedfde0ad29dfcc252dba066
Kim Minjae <ffalswo2@naver.com>
2022-09-07 23:46:44 +0900

    프로그래머스 Level2 큰수만들기

---
+++ algo/프로그래머스/Level2/큰수만들기.swift
@@ -0,0 +1,37 @@
+//
+//  큰수만들기.swift
+//  algo
+//  https://school.programmers.co.kr/learn/courses/30/lessons/42883/solution_groups?language=swift
+//  Created by 김민재 on 2022/09/07.
+//
+
+import Foundation
+
+
+func solution(_ number:String, _ k:Int) -> String {
+    let number = number.map { String($0) }
+    
+    var k = k
+    var stack: [String] = []
+    
+    
+    for (idx, num) in number.enumerated() {
+        while k > 0 && !stack.isEmpty && stack.last! < num {
+            stack.removeLast()
+            k -= 1
+        }
+        
+        if k > 0 {
+            stack.append(num)
+        } else {
+            stack += number[idx...]
+            break
+        }
+    }
+    
+    return String(stack.joined().prefix(stack.count - k))
+}
+
+_ = solution("1924", 2)
+_ = solution("1231234", 3)
+_ = solution("4177252841", 4)
