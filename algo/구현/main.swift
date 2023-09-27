14c5c0f80261a4853dfb25e6954ce3697967a740
Kim Minjae <ffalswo2@naver.com>
2023-09-28 01:33:18 +0900

    퍼즐조각 맞추기 완

--- algo/구현/main.swift
+++ algo/구현/main.swift
@@ -1,5 +1,5 @@
 //
-//  감시(2).swift
+//  main.swift
 //  algo
 //
 //  Created by 김민재 on 9/27/23.
@@ -7,105 +7,144 @@
 
 import Foundation
 
-let dy = [-1, 0, 1, 0] // 상 우 하 좌
-let dx = [0, 1, 0, -1]
+func rotate(sp: [[Int]]) -> [[Int]] {
+    var tmp = sp
 
-var cameras: [(Int, Int, Int)] = [] // cctv카메라 좌표를 저장할 공간
-
-var board: [[Int]] = []
-
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let n = input[0]
-let m = input[1]
-
-for _ in 0..<n {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-    board.append(input)
-}
-
-
-var answer = 0
-var candidates = 1
-for i in 0..<n {
-    for j in 0..<m {
-        if board[i][j] >= 1 && board[i][j] < 6 {
-            cameras.append((board[i][j], i, j))
-            candidates *= 4
-        } else if board[i][j] == 0 {
-            answer += 1
+    for i in 0..<sp.count {
+        for j in 0..<sp.count {
+            tmp[i][sp.count - j - 1] = sp[j][i]
         }
     }
+    return tmp
+
 }
 
-var checkedAreaCnt = 0
 
-func checkCamArea(board: inout [[Int]], y:Int, x: Int, dir: Int) {
-    let dir = dir % 4
+func isFit(sp: [(Int, Int)], piece: [(Int, Int)]) -> Bool {
+    var tmp = Array(repeating: Array(repeating: 0, count: 10), count: 10)
 
-    var ny = y + dy[dir]
-    var nx = x + dx[dir]
+    var startY = tmp.count / 2
 
-    while true {
-        if ny < 0 || nx < 0 || ny >= n || nx >= m || board[ny][nx] == 6 { break }
-        else if board[ny][nx] > 0 { continue }
-        board[ny][nx] = -1 // # 표시 대신 -1
-        checkedAreaCnt += 1
-
-        ny += dy[dir]
-        nx += dx[dir]
+    for s in sp {
+        tmp[s.0 + startY][s.1 + startY] = 1
     }
+
+
+    for _ in 0..<4 {
+        tmp = rotate(sp: tmp)
+
+    check: for i in 0..<tmp.count {
+            for j in 0..<tmp.count {
+                if tmp[i][j] == 1 {
+                    var flag = true
+                    for p in piece {
+                        if tmp[p.0 + i][p.1 + j] == 0 {
+                            flag = false
+                            break check
+                        }
+                    }
+
+                    if flag {
+                        return true
+                    }
+                }
+            }
+
+        }
+    }
+
+
+    return false
 }
 
-for can in 0..<candidates {
-    var initZeros = answer
-    var tmpBoard = board
+
+func solution(_ game_board:[[Int]], _ table:[[Int]]) -> Int {
+
+    let n = game_board.count
+
+    var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
+
+
+    var spaces: [[(Int, Int)]] = [] // game_board 빈칸 정보 찾기
+    var pieces: [[(Int, Int)]] = [] // table 조각 정보 찾기
+
+    let dy = [1, -1, 0, 0]
+    let dx = [0, 0, 1, -1]
+
+    func bfs(board: [[Int]], y: Int, x: Int, type: Int) -> [(Int, Int)] {
+        
+        var space: [(Int, Int)] = [(0, 0)]
+        var q = [(0, 0)]
+        visited[y][x] = true
+        var qIndex = 0
+
+
+        while qIndex < q.count {
+            let cury = q[qIndex].0
+            let curx = q[qIndex].1
+            qIndex += 1
+
+            for i in 0..<4 {
+                let ny = y + cury + dy[i]
+                let nx = x + curx + dx[i]
+
+                if 0..<n ~= ny && 0..<n ~= nx && !visited[ny][nx] && board[ny][nx] == type {
+                    visited[ny][nx] = true
+                    q.append((ny - y, nx - x))
+                    space.append((ny - y, nx - x))
+                }
+            }
+
+        }
+
+        return space
+    }
+
     
-    var tmp = can
-    for cIdx in 0..<cameras.count {
-        let dir = tmp % 4
-        tmp /= 4
-
-        let camera = cameras[cIdx].0
-        let y = cameras[cIdx].1
-        let x = cameras[cIdx].2
-
-        if camera == 1 {
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir)
-
-        } else if camera == 2 {
-            if dir == 2 || dir == 3 { continue }
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir)
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir + 2)
-
-        } else if camera == 3 {
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir)
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir + 1)
-
-        } else if camera == 4 {
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir)
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir + 1)
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir + 2)
-        } else if camera == 5 {
-            if dir != 0 { continue }
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir)
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir + 1)
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir + 2)
-            checkCamArea(board: &tmpBoard, y: y, x: x, dir: dir + 3)
-        }
-
-    }
-
-    var zeroCnt = 0
     for i in 0..<n {
-        for j in 0..<m {
-            if tmpBoard[i][j] == 0 {
-                zeroCnt += 1
+        for j in 0..<n {
+            if game_board[i][j] == 0 && !visited[i][j] {
+                let sp = bfs(board: game_board, y: i, x: j, type: 0)
+                spaces.append(sp)
             }
         }
     }
-    
-    answer = min(answer, checkedAreaCnt)
-    checkedAreaCnt = 0
+    // game_table에서 빈칸 다 찾음
+
+    visited = Array(repeating: Array(repeating: false, count: n), count: n)
+
+    for i in 0..<n {
+        for j in 0..<n {
+            if table[i][j] == 1 && !visited[i][j] {
+                let piece = bfs(board: table, y: i, x: j, type: 1)
+                pieces.append(piece)
+            }
+        }
+    }
+    // table에서 조각 다 찾음
+
+    var answer = 0
+    var spaceFilled = Array(repeating: false, count: spaces.count)
+    var pieceUsed = Array(repeating: false, count: pieces.count)
+
+    for sIdx in 0..<spaces.count {
+        let space = spaces[sIdx]
+
+        for pIdx in 0..<pieces.count {
+            let piece = pieces[pIdx]
+            if space.count != piece.count || spaceFilled[sIdx] || pieceUsed[pIdx] { continue }
+
+            if isFit(sp: space, piece: piece) {
+                spaceFilled[sIdx] = true
+                pieceUsed[pIdx] = true
+                answer += space.count
+            }
+        }
+    }
+
+    return answer
 }
 
-print(answer)
+
+solution([[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]]    ,
+         [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]]    )
